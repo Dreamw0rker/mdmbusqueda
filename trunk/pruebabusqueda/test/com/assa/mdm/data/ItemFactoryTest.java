692ca61ae5c0005277ccd222290f771c77706143
theo.grip <theo.grip@3db85dd3-6649-6fbc-07fc-2292f85b6d4a>
2010-09-06 21:34:12 +0000

    Finish item factory

--- trunk/pruebabusqueda/test/com/assa/mdm/data/ItemFactoryTest.java
+++ trunk/pruebabusqueda/test/com/assa/mdm/data/ItemFactoryTest.java
@@ -8,36 +8,73 @@ import static org.mockito.Mockito.when;
 import mx.com.mypo.bpd.caf.catalogoproductos.Item;
 
 import org.junit.Test;
+import org.mockito.Mock;
 
+import com.assa.test.BaseMockitoTest;
 import com.sap.mdm.data.Record;
 import com.sap.mdm.ids.FieldId;
 import com.sap.mdm.session.UserSessionContext;
 import com.sap.mdm.valuetypes.StringValue;
 
-public class ItemFactoryTest {
+public class ItemFactoryTest extends BaseMockitoTest {
+	@Mock
+	private Record productRecord;
+	@Mock
+	private UserSessionContext userCtx;
+	@Mock
+	private Repository repository;
+	
 	@Test
 	public void shouldConvertRecordToItem() throws Exception {
-		Record product = mock(Record.class);
-		UserSessionContext userCtx = mock(UserSessionContext.class);
-		Repository repository = mock(Repository.class);
 		String numeroMaterial = "123";
-		FieldId numeroMaterialFieldId = mockFieldId(product, userCtx, repository, Product.FIELD_NUMERO_MATERIAL, numeroMaterial);
-
+		String descLarga = "Azomatlin, 42 mg";
+		String precioMinimo = "223";
+		String presentacion = "Comercial";
+		String empaque = "Empaque";
+		String unidad = "Caja";
+		
+		FieldId numeroMaterialFieldId = mockStringValue(Product.FIELD_NUMERO_MATERIAL, numeroMaterial);
+		FieldId descLargaFieldId = mockStringValue(Product.FIELD_DESC_LARGA, descLarga);
+		FieldId precioCompraFieldId = mockLookup(Product.FIELD_PREC_COMPRA, precioMinimo);
+		FieldId presentacionFieldId = mockLookup(Product.FIELD_PRESENTACION, presentacion);
+		FieldId empaqueField = mockLookup(Product.FIELD_EMPAQUE, empaque);
+		FieldId unidadField = mockLookup(Product.FIELD_UNIDAD, unidad);
+		
 		ItemFactory itemFactory = new ItemFactory();
-		Item item = itemFactory.toItem(product);
+		Item item = itemFactory.toItem(productRecord);
 
 		assertNotNull(item);
-		verify(product).getFieldValue(numeroMaterialFieldId);
+		verify(productRecord).getFieldValue(numeroMaterialFieldId);
+		verify(productRecord).getFieldValue(descLargaFieldId);
+		verify(productRecord).getLookupDisplayValue(precioCompraFieldId);
+		verify(productRecord).getLookupDisplayValue(presentacionFieldId);
+		verify(productRecord).getLookupDisplayValue(empaqueField);
+		verify(productRecord).getLookupDisplayValue(unidadField);
 		assertEquals(numeroMaterial, item.getClaveProducto());
+		assertEquals(descLarga, item.getDescripcion());
+		assertEquals(precioMinimo, item.getPMR());
+		assertEquals(presentacion, item.getPresentacion());
+		assertEquals(empaque, item.getPresentacionEmpaque());
+		assertEquals(unidad, item.getUnidadMedida());
 	}
 
-	private FieldId mockFieldId(Record productRecord, UserSessionContext userCtx, Repository repository, Product product, String value) {
+	private FieldId mockStringValue(Product product, String value) {
+		FieldId numeroMaterialFieldId = getFieldId(product);
+		when(productRecord.getFieldValue(numeroMaterialFieldId)).thenReturn(new StringValue(value));
+		return numeroMaterialFieldId;
+	}
+
+	private FieldId getFieldId(Product product) {
 		FieldId numeroMaterialFieldId = mock(FieldId.class);
 		when(repository.getFieldId(userCtx, Product.TABLE_NAME.toString(), product.toString())).
 			thenReturn(numeroMaterialFieldId);
-		StringValue stringValue = new StringValue(value);
-		when(productRecord.getFieldValue(numeroMaterialFieldId)).thenReturn(stringValue);
 		product.initFieldId(repository, userCtx);
 		return numeroMaterialFieldId;
+	}
+	
+	private FieldId mockLookup(Product product, String value) {
+		FieldId fieldId = getFieldId(product);
+		when(productRecord.getLookupDisplayValue(fieldId)).thenReturn(value);
+		return fieldId;
 	}
 }
