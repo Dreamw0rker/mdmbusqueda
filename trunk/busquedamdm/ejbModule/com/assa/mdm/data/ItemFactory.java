a4e66336e3d3ad4c08bdb7cbd12dbd59bc6b8df6
theo.grip <theo.grip@3db85dd3-6649-6fbc-07fc-2292f85b6d4a>
2010-09-06 21:34:16 +0000

    Finish item factory

--- trunk/busquedamdm/ejbModule/com/assa/mdm/data/ItemFactory.java
+++ trunk/busquedamdm/ejbModule/com/assa/mdm/data/ItemFactory.java
@@ -13,6 +13,12 @@ public class ItemFactory {
 		FieldId fieldId = Product.FIELD_NUMERO_MATERIAL.getFieldId();
 		StringValue stringValue = (StringValue) product.getFieldValue(fieldId);
 		item.setClaveProducto(stringValue.getString());
+		stringValue = (StringValue) product.getFieldValue(Product.FIELD_DESC_LARGA.getFieldId());
+		item.setDescripcion(stringValue.getString());
+		item.setPMR(product.getLookupDisplayValue(Product.FIELD_PREC_COMPRA.getFieldId()));
+		item.setPresentacion(product.getLookupDisplayValue(Product.FIELD_PRESENTACION.getFieldId()));
+		item.setPresentacionEmpaque(product.getLookupDisplayValue(Product.FIELD_EMPAQUE.getFieldId()));
+		item.setUnidadMedida(product.getLookupDisplayValue(Product.FIELD_UNIDAD.getFieldId()));
 		return item;		
 	}
 
