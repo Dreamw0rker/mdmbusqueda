2902bc89b1cf43bbd6c6b5a2656b15e6b32a478a
theo.grip <theo.grip@3db85dd3-6649-6fbc-07fc-2292f85b6d4a>
2010-09-08 23:51:39 +0000

    add new values to item and change schema

--- trunk/pruebabusqueda/test/com/assa/mdm/ejb/BuscadorTest.java
+++ trunk/pruebabusqueda/test/com/assa/mdm/ejb/BuscadorTest.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import mx.com.mypo.bpd.caf.catalogoproductos.Item;
 import mx.com.mypo.bpd.caf.catalogoproductos.SubItem;
 
 import org.junit.Before;
@@ -34,6 +35,7 @@ import com.sap.mdm.session.UserSessionContext;
 import com.sap.mdm.valuetypes.StringValue;
 
 public class BuscadorTest extends BaseMockitoTest {
+	private static final String PARTIDA = "22";
 	@InjectMocks
 	private Buscador buscador = new Buscador();
 	@Mock
@@ -57,6 +59,8 @@ public class BuscadorTest extends BaseMockitoTest {
 	private RecordResultSet recordSet;
 	@Mock
 	private Record record;
+	@Mock
+	private Item item;
 	
 	@Before
 	public void configureMocks() throws MdmException {
@@ -69,30 +73,34 @@ public class BuscadorTest extends BaseMockitoTest {
 		when(recordSet.getRecords()).thenReturn(records );
 		StringValue stringValue = mock(StringValue.class);
 		when(record.getFieldValue(any(FieldId.class))).thenReturn(stringValue);
+		when(itemFactory.toItemPadre(record)).thenReturn(item);
 	}
 	
 	@Test
 	public void shouldGetCommands() throws Exception {
 		Map<Product, String> parametrosBusqueda = new HashMap<Product, String>();
-		parametrosBusqueda.put(Product.FIELD_DESC_LARGA, "AR");
-		List<SubItem> products = buscador.findProducts(parametrosBusqueda);
+		parametrosBusqueda.put(Product.FIELD_DESC, "AR");
+		
+		List<SubItem> products = buscador.findProducts(parametrosBusqueda, PARTIDA);
 		assertEquals(1, products.size());
+//		assertEquals(PARTIDA, products.get(0).getItemPadre().getPartida());
 		
 		verify(mockMdmConnection).getUserContext();
 		verify(mockCommandFactory).getLimitedRecordsCommand(eq(userCtx), anyString());
-		verify(itemFactory, times(2)).toItem(record);
+		verify(itemFactory, times(2)).toItemPadre(record);
 		//two for padre, one for hijo
 		verify(search, times(3)).addSearchItem(isA(SearchDimension.class), isA(SearchConstraint.class));
+		verify(item).setPartida(PARTIDA);
 	}
 	
 	@Test
 	public void shouldAddAllSearchParameters() throws Exception {
 		Map<Product, String> parametrosBusqueda = new HashMap<Product, String>();
-		parametrosBusqueda.put(Product.FIELD_DESC_LARGA, "AR");
+		parametrosBusqueda.put(Product.FIELD_DESC, "AR");
 		parametrosBusqueda.put(Product.FIELD_EMPAQUE, "Caja");
 		parametrosBusqueda.put(Product.FIELD_NUMERO_MATERIAL, "234");
 		
-		buscador.findProducts(parametrosBusqueda);
+		buscador.findProducts(parametrosBusqueda, PARTIDA);
 		//3 parameters, and 2 for being a father plus children
 		verify(search, times(5)).addSearchItem(isA(SearchDimension.class), isA(SearchConstraint.class));
 	}
@@ -103,7 +111,7 @@ public class BuscadorTest extends BaseMockitoTest {
 		parametrosBusqueda.put(Product.FIELD_EMPAQUE, "Caja");
 		parametrosBusqueda.put(Product.FIELD_NUMERO_MATERIAL, "234");
 		
-		buscador.findProducts(parametrosBusqueda);
+		buscador.findProducts(parametrosBusqueda, PARTIDA);
 		//2 parameters, and 2 for being a father plus children
 		verify(search, times(4)).addSearchItem(isA(SearchDimension.class), isA(SearchConstraint.class));
 	}
