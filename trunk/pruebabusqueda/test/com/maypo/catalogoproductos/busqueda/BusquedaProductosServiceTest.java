d7e28221bef375feec8b0bde34bdd91c6189c712
theo.grip <theo.grip@3db85dd3-6649-6fbc-07fc-2292f85b6d4a>
2010-09-08 18:01:41 +0000

    Cambiar para buscar por mas de un parametro

--- trunk/pruebabusqueda/test/com/maypo/catalogoproductos/busqueda/BusquedaProductosServiceTest.java
+++ trunk/pruebabusqueda/test/com/maypo/catalogoproductos/busqueda/BusquedaProductosServiceTest.java
@@ -1,32 +1,88 @@
 package com.maypo.catalogoproductos.busqueda;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Map;
+
 import mx.com.mypo.bpd.caf.catalogoproductos.BusquedaProductosFault_Exception;
 import mx.com.mypo.bpd.caf.catalogoproductos.BusquedaProductosQuery;
 import mx.com.mypo.bpd.caf.catalogoproductos.BusquedaProductosServiceImplBean;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import com.assa.mdm.data.Product;
 import com.assa.mdm.ejb.BuscadorLocal;
 import com.assa.test.BaseMockitoTest;
 import com.sap.mdm.MdmException;
 
 
+@SuppressWarnings("unchecked")
 public class BusquedaProductosServiceTest extends BaseMockitoTest {
 	@InjectMocks
 	private BusquedaProductosServiceImplBean busquedaService = new BusquedaProductosServiceImplBean();
 	@Mock
 	private BuscadorLocal buscador;
+	private BusquedaProductosQuery busquedaProductosRequest = new BusquedaProductosQuery();
+	private String descripcion = "ARZ";
+	
+	@Test
+	public void shouldConfigureBusquedaWithDescripcion() throws Exception {
+		busquedaProductosRequest.setDescripcion(descripcion);
+		Map<Product, String> parametrosBusqueda = executeTest();
+		verifyParameter(parametrosBusqueda, Product.FIELD_DESC_LARGA, descripcion);
+	}
+
+	private void verifyParameter(Map<Product, String> parametrosBusqueda, Product key, String value) {
+		assertEquals(1, parametrosBusqueda.size());
+		assertTrue(parametrosBusqueda.containsKey(key));
+		assertTrue(parametrosBusqueda.containsValue(value));
+	}
+
+	private Map<Product, String> executeTest() throws BusquedaProductosFault_Exception, MdmException {
+		busquedaService.buscarProductos(busquedaProductosRequest);
+		ArgumentCaptor<Map> busquedaMap = ArgumentCaptor.forClass(Map.class);
+		verify(buscador).findProducts(busquedaMap.capture());
+		Map<Product, String> parametrosBusqueda = busquedaMap.getValue();
+		return parametrosBusqueda;
+	}
+	
+	@Test
+	public void shouldCallBusquedaWithClave() throws Exception {
+		String clave = "345";
+		busquedaProductosRequest.setClave(clave);
+		Map<Product, String> parametrosBusqueda = executeTest();
+		verifyParameter(parametrosBusqueda, Product.FIELD_NUMERO_MATERIAL, clave);
+	}
+	
+	@Test
+	public void shouldCallBusquedaWithEmpaque() throws Exception {
+		String empaque = "Caja";
+		busquedaProductosRequest.setEmpaque(empaque);
+		Map<Product, String> parametrosBusqueda = executeTest();
+		verifyParameter(parametrosBusqueda, Product.FIELD_EMPAQUE, empaque);
+	}
+	
+	@Test
+	public void shouldCallBusquedaWithThreeParameters() throws Exception {
+		busquedaProductosRequest.setDescripcion(descripcion);
+		busquedaProductosRequest.setClave("345");
+		busquedaProductosRequest.setEmpaque("Caja");
+		
+		Map<Product, String> parametrosBusqueda = executeTest();
+		assertEquals(3, parametrosBusqueda.size());
+		assertTrue(parametrosBusqueda.containsKey(Product.FIELD_NUMERO_MATERIAL));
+	}
 	
 	@Test(expected=BusquedaProductosFault_Exception.class)
 	public void shouldThrowBusquedaFaultExcecion() throws Exception {
-		BusquedaProductosQuery busquedaProductosRequest = new BusquedaProductosQuery();
-		String descripcion = "ARZ";
 		busquedaProductosRequest.setDescripcion(descripcion);
-		when(buscador.findProducts(descripcion)).thenThrow(new MdmException());
+		when(buscador.findProducts(isA(Map.class))).thenThrow(new MdmException());
 		
 		busquedaService.buscarProductos(busquedaProductosRequest);
 	}
