bffaf79502e1ca076561abc0502a2e86e4b6f1b0
theo.grip <theo.grip@3db85dd3-6649-6fbc-07fc-2292f85b6d4a>
2010-09-20 23:47:54 +0000

    add taxonomy search

--- pruebabusqueda/test/com/assa/mdm/data/AtributosTest.java
+++ pruebabusqueda/test/com/assa/mdm/data/AtributosTest.java
@@ -17,9 +17,8 @@ public class AtributosTest {
 		AttributeId attributeId = mock(AttributeId.class);
 		when(repository.getAttributeId(eq(userCtx), anyString())).thenReturn(attributeId);
 		Atributo atributo = Atributo.SUBSTANCIA_ACTIVA;
-		atributo.initAttributeId(repository, userCtx);
 		
+		assertEquals(attributeId, atributo.initAttributeId(repository, userCtx));
 		verify(repository).getAttributeId(userCtx, atributo.toString());
-		assertEquals(attributeId, atributo.getAttributeId());
 	}
 }
--- pruebabusqueda/test/com/assa/mdm/ejb/BuscadorTest.java
+++ pruebabusqueda/test/com/assa/mdm/ejb/BuscadorTest.java
@@ -13,11 +13,13 @@ import mx.com.mypo.bpd.caf.catalogoproductos.SubItem;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import com.assa.mdm.command.CommandFactory;
 import com.assa.mdm.connection.MDMConnection;
+import com.assa.mdm.data.Atributo;
 import com.assa.mdm.data.ItemFactory;
 import com.assa.mdm.data.Product;
 import com.assa.mdm.data.Repository;
@@ -28,6 +30,7 @@ import com.sap.mdm.data.RecordResultSet;
 import com.sap.mdm.data.commands.RetrieveLimitedRecordsCommand;
 import com.sap.mdm.extension.data.ResultDefinitionEx;
 import com.sap.mdm.ids.FieldId;
+import com.sap.mdm.search.AttributeSearchDimension;
 import com.sap.mdm.search.Search;
 import com.sap.mdm.search.SearchConstraint;
 import com.sap.mdm.search.SearchDimension;
@@ -35,7 +38,6 @@ import com.sap.mdm.session.UserSessionContext;
 import com.sap.mdm.valuetypes.StringValue;
 
 public class BuscadorTest extends BaseMockitoTest {
-	private static final String PARTIDA = "22";
 	@InjectMocks
 	private Buscador buscador = new Buscador();
 	@Mock
@@ -81,16 +83,14 @@ public class BuscadorTest extends BaseMockitoTest {
 		Map<Product, String> parametrosBusqueda = new HashMap<Product, String>();
 		parametrosBusqueda.put(Product.FIELD_DESC, "AR");
 		
-		List<SubItem> products = buscador.findProducts(parametrosBusqueda, PARTIDA);
+		List<SubItem> products = buscador.findProducts(parametrosBusqueda, new HashMap<Atributo, String>());
 		assertEquals(1, products.size());
-//		assertEquals(PARTIDA, products.get(0).getItemPadre().getPartida());
 		
 		verify(mockMdmConnection).getUserContext();
 		verify(mockCommandFactory).getLimitedRecordsCommand(eq(userCtx), anyString());
 		verify(itemFactory, times(2)).toItemPadre(record);
 		//two for padre, one for hijo
 		verify(search, times(3)).addSearchItem(isA(SearchDimension.class), isA(SearchConstraint.class));
-		verify(item).setPartida(PARTIDA);
 	}
 	
 	@Test
@@ -100,7 +100,7 @@ public class BuscadorTest extends BaseMockitoTest {
 		parametrosBusqueda.put(Product.FIELD_EMPAQUE, "Caja");
 		parametrosBusqueda.put(Product.FIELD_NUMERO_MATERIAL, "234");
 		
-		buscador.findProducts(parametrosBusqueda, PARTIDA);
+		buscador.findProducts(parametrosBusqueda, new HashMap<Atributo, String>());
 		//3 parameters, and 2 for being a father plus children
 		verify(search, times(5)).addSearchItem(isA(SearchDimension.class), isA(SearchConstraint.class));
 	}
@@ -111,8 +111,40 @@ public class BuscadorTest extends BaseMockitoTest {
 		parametrosBusqueda.put(Product.FIELD_EMPAQUE, "Caja");
 		parametrosBusqueda.put(Product.FIELD_NUMERO_MATERIAL, "234");
 		
-		buscador.findProducts(parametrosBusqueda, PARTIDA);
+		buscador.findProducts(parametrosBusqueda, new HashMap<Atributo, String>());
 		//2 parameters, and 2 for being a father plus children
 		verify(search, times(4)).addSearchItem(isA(SearchDimension.class), isA(SearchConstraint.class));
 	}
+	
+	@Test
+	public void shouldSearchForAttributes() throws Exception {
+		Map<Atributo, String> atributos = new HashMap<Atributo, String>();
+		Atributo attribute = Atributo.SUBSTANCIA_ACTIVA;
+		atributos.put(attribute, "Zoxil");
+		
+		buscador.findProducts(new HashMap<Product, String>(), atributos);
+		ArgumentCaptor<SearchDimension> searchDimensionArg = ArgumentCaptor.forClass(SearchDimension.class);
+		verify(search, times(3)).addSearchItem(searchDimensionArg.capture(), isA(SearchConstraint.class));
+		
+		SearchDimension searchDimension = searchDimensionArg.getAllValues().get(0);
+		assertTrue(searchDimension instanceof AttributeSearchDimension);
+		AttributeSearchDimension attributeSearchDimension = (AttributeSearchDimension) searchDimension;
+		assertEquals(attribute.getAttributeId(), attributeSearchDimension.getAttributeId());
+		FieldId[] fieldPath = attributeSearchDimension.getFieldPath();
+		assertEquals(1, fieldPath.length);
+		assertEquals(Product.FIELD_TAX_GRUPO_ART.getFieldId(), fieldPath[0]);
+	}
+	
+	@Test
+	public void shouldAddTwoSearchAttributes() throws Exception {
+		Map<Atributo, String> atributos = new HashMap<Atributo, String>();
+		atributos.put(Atributo.SUBSTANCIA_ACTIVA, "Zoxil");
+		atributos.put(Atributo.CONCENTRACION, "50 mg");
+		
+		buscador.findProducts(new HashMap<Product, String>(), atributos);
+		
+		verify(search, times(4)).addSearchItem(isA(SearchDimension.class), isA(SearchConstraint.class));
+	}
+	
+	
 }
