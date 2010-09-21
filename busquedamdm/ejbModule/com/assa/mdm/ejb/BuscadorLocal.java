7e6e56d74a665706adfaee113013e5c83a0e18be
theo.grip <theo.grip@3db85dd3-6649-6fbc-07fc-2292f85b6d4a>
2010-09-21 22:56:18 +0000

    get conection properties from destination

--- busquedamdm/ejbModule/com/assa/mdm/ejb/BuscadorLocal.java
+++ busquedamdm/ejbModule/com/assa/mdm/ejb/BuscadorLocal.java
@@ -1,5 +1,6 @@
 package com.assa.mdm.ejb;
 
+import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
 
@@ -10,10 +11,11 @@ import mx.com.mypo.bpd.caf.catalogoproductos.SubItem;
 import com.assa.mdm.data.Atributo;
 import com.assa.mdm.data.Product;
 import com.sap.mdm.MdmException;
+import com.sap.security.core.server.destinations.api.DestinationException;
 
 
 @Local
 public interface BuscadorLocal {
-	List<SubItem> findProducts(Map<Product, String> parametrosBusqueda, Map<Atributo,String> atributos) throws MdmException;
+	List<SubItem> findProducts(Map<Product, String> parametrosBusqueda, Map<Atributo,String> atributos) throws MdmException, Exception;
 	
 }
